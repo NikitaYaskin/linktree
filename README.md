149aac0b27e0f1a73e93330e11a148c6ab30023c
nikita <nikuuu66@gmail.com>
2022-07-11 15:40:10 +0300

    Adding navbar into a Linktree

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* Ruby version 3.1.0
 
-* System dependencies
+* Rails version 7.0.3
+
+* System dependencies: Devise, Simple_Form 
 
 * Configuration
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'dashboard/index', as: :dashboard
   root 'dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
