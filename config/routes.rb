66c474f37026d96f615f62e7e988e9bf045343b3
nikita <nikuuu66@gmail.com>
2022-07-15 21:14:45 +0300

    Added avatar for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'dashboard/index', as: :dashboard
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' 
+  }
+  get 'dashboard', to: 'dashboard#index'
+  get 'appearance', to: 'dashboard#appearance'
   root 'dashboard#index'
+
+  # Allow us to use user_path(user) to get the URL for a user's profile page 
+  get ':id', to: 'dashboard#show', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
